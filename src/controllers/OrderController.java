b6e2d126fc4c17cb9025b536da7c661be4618cbb
israelviana <rael232is@gmail.com>
2023-09-19 21:27:25 -0300

    livraria

--- src/controllers/OrderController.java
+++ src/controllers/OrderController.java
@@ -1,9 +1,12 @@
 package controllers;
 import dtos.OrderDTO;
 import dtos.ClientDTO;
+import services.Discount;
 
 public class OrderController {
     public void createOrder(ClientDTO client, OrderDTO order){
+        Discount discount = new Discount();
+        discount.calculateDiscount(order);
         System.out.println("criando pedido");
     }
 
--- src/services/Discount.java
+++ src/services/Discount.java
@@ -5,9 +5,7 @@ import dtos.OrderDTO;
 public class Discount {
     public double calculateDiscount(OrderDTO order) {
         double discount = 0.0;
-
         // Lógica para calcular desconto com base em promoções sazonais
-
         return discount;
     }
 
